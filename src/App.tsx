6b9e8434576493f83f13b5f57ae39d99ae147c6e
justinJSwebdev <trankyhung225@gmail.com>
2024-02-07 23:00:41 +0700

    update components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+import { useState } from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
 import Tooltip from './components/Tooltip/Tooltip'
 import UserIcon from './components/icons/UserIcon'
+import Modal from './components/Modal/Modal'
 
 function App() {
+  const [show, setShow] = useState<boolean>(false)
+  const [show2, setShow2] = useState<boolean>(true)
+  const [count, setCount] = useState(1)
   return (
     <>
-      <Tooltip parentElement={<UserIcon></UserIcon>} title='This is user Icon'></Tooltip>
+      <Sidebar isOpen={show} onSidebarClose={() => setShow(false)}>
+        1
+      </Sidebar>
+      <button onClick={() => setShow(true)}>push</button>
+      {count}
     </>
   )
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,50 @@
-export default function Sidebar() {
-  return <div>Sidebar</div>
+import { createPortal } from 'react-dom'
+
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+interface ISidebarProps {
+  isOpen: boolean
+  children: React.ReactNode
+  className?: string
+  bodyClassName?: string
+  title?: string
+  onSidebarClose: () => void
+}
+
+export default function Sidebar({
+  children,
+  isOpen = false,
+  className = '',
+  bodyClassName = '',
+  title = '',
+  onSidebarClose
+}: ISidebarProps) {
+  return createPortal(
+    <>
+      <div className={`fixed inset-0 z-40 ${!isOpen ? 'pointer-events-none' : ' pointer-events-auto'}`}>
+        <div
+          className={`absolute inset-0 bg-black bg-opacity-30 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} transition-all duration-300`}
+          onClick={onSidebarClose}
+        ></div>
+        <div
+          className={`fixed top-0 right-0 min-h-screen z-50 p-10 px-4 w-[50vw] lg:w-[35vw] bg-white ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-all duration-300 ease-in-out ${bodyClassName}`}
+        >
+          <div className='flex justify-end px-10'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-8 h-8 cursor-pointer'
+              onClick={onSidebarClose}
+            >
+              <path strokeLinecap='round' strokeLinejoin='round' d='M6 18 18 6M6 6l12 12' />
+            </svg>
+          </div>
+          <div className='p-5'>{children}</div>
+        </div>
+      </div>
+    </>,
+    document.body as HTMLElement
+  )
 }
